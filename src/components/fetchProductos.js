c366518c3ec679a2ff6a7a263924d34cec3932be
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2024-04-02 18:38:17 -0300

    problema icono whatsapp y logo solucionado

--- src/components/fetchProductos.js
+++
@@ -1,6 +0,0 @@
-export async function fetchProductos() {
-    const response = await fetch('/src/components/productos.json');
-    const productos = await response.json();
-    return { productos };
-  }
-  
